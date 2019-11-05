02df92bd0907d768060bbaa65fdb54b31007fdba
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2019-11-05 15:33:15 +0100

    minor changes

--- frontend/src/actions/sessionActions.ts
+++ frontend/src/actions/sessionActions.ts
@@ -4,18 +4,9 @@ import MyPeer from '../peerjs';
 export const SET_PEER = 'set peer';
 export const SET_SESSION = 'set session';
 
-// export const setPeer = (peer, peerId) => ({
-//   type: SET_PEER,
-//   peer,
-//   peerId,
-// });
-
 export const setPeer = (peer: MyPeer, peerId: string) =>
   action(SET_PEER, peer, peerId);
 
 export const setSession = (login: string) => action(SET_SESSION, login);
 
-// export const setSession = (login) => ({
-//   type: SET_SESSION,
-//   payload: login
-// });
+export type SessionAction = typeof setPeer | typeof setSession;
